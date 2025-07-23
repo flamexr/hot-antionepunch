220e4c953e66b358e81cc980863c11914efb9f89
Flamè Sânto <barankunur81@gmail.com>
2025-07-24 00:04:18 +0300

    Add files via upload

---
+++ hot-antionepunch/config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+
+Config.AntiCitizenOnePunch = true -- TRUE/FALSE
+
+Config.Fiveguardban = false -- IF FALSE KICKS ONLY
+
+Config.Fiveguardname = "venus-balikci" -- ENTER FIVEGUARD NAME
+
+
+Config.LogWebhook = "WEBHOOK_URL" -- FOR LOG SYSTEM
